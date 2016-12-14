82516b433c18a222972c8e8beaa9fd666ecaba32
SparkLee <liweijsj@163.com>
2016-12-14 12:07:34 +0800

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -10,6 +10,49 @@
 /*                                             */
 /* =========================================== */
 
+define('SPK_DO_LOG', TRUE);
+define('SPK_LOG_DIR', dirname(__FILE__) . '/logs/');
+
+if (!function_exists('spklog')) {
+    /**
+     * 写日志（只有配置文件\app\config\app.php中的配置项spklog为true时，才会写日志）
+     * @author SparkLee
+     * @since 2015/07/31 10:28
+     * @param unknown $logfile_name 带子目录的文件名（如：ad/ad.log），注：文件扩展名必须.log
+     * @param unknown $content 日志内容
+     * @param string $flags 内容写入方式（覆盖，追加等）
+     *  Available flags
+     *   Flag Description
+     *   FILE_USE_INCLUDE_PATH  Search for filename in the include directory. See include_path for more information.
+     *   FILE_APPEND  If file filename already exists, append the data to the file instead of overwriting it.
+     *   LOCK_EX  Acquire an exclusive lock on the file while proceeding to the writing.
+     */
+    function spklog($logfile_name, $data, $title='', $log_level = 'INFO',  $flags = FILE_APPEND) {
+        if (SPK_DO_LOG) {
+            $index_of_last_slash = strrpos($logfile_name, '/');// 最后一个斜杠的位置
+            $dir = SPK_LOG_DIR . substr($logfile_name, 0, $index_of_last_slash);            
+            if (!is_dir($dir)) {
+                mkdir($dir, 0777, true); // 设置第三个参数$recursive为true，递归创建目录
+            }
+            $file_path = str_replace('.log', '-' . date('Y-m-d') . '.log', SPK_LOG_DIR . $logfile_name); // 把文件名a.log替换成a-2015-10-09.log
+
+            // 非字符串的，一律转换为json字符串
+            if(!is_string($data)) {
+            	$data = json_encode($data, JSON_UNESCAPED_UNICODE);
+            }
+
+            $data = date('Y-m-d H:i:s') . " [{$log_level}] {$title} - " . $data . "\r\n";
+
+            // sdk目录下使用spklog时（sdk客户端发HTTP请求打日志），日志文件中若有中文则会乱码（使用mb_http_input('I')可以检测HTTP输入字符编码），采用以下方式可以解决乱码问题
+            // 如果你在 out_charset 后添加了字符串 //TRANSLIT，将启用转写（transliteration）功能。这个意思是，当一个字符不能被目标字符集所表示时，它可以通过一个或多个形似的字符来近似表达。 如果你添加了字符串 //IGNORE，不能以目标字符集表达的字符将被默默丢弃。 否则，str 从第一个无效字符开始截断并导致一个 E_NOTICE。
+            $data = iconv('UTF-8', "GB2312//IGNORE", $data);
+            $data = iconv('GB2312', "UTF-8//IGNORE", $data);
+
+            file_put_contents($file_path, $data, $flags);
+        }
+    }
+}
+
 if (!function_exists('spk_dd')) {
     /**
      * 打印给定内容并结束脚本
