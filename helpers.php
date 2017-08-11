aace96d30a2befa117e39b2fdae7cda532654649
SparkLee <liweijsj@163.com>
2017-08-11 17:34:37 +0800

    添加函数：获取客户端IP地址、根据IP地址获取地理位置（国家、地区、省、市、运营商）

--- helpers.php
+++ helpers.php
@@ -223,6 +223,54 @@ if(!function_exists('spk_get_http_response_post')) {
     }
 }
 
+if(!function_exists('spk_get_client_ip')) {
+    /**
+     * 获取客户端IP地址
+     * @param int  $type 返回类型 0 返回IP地址 1 返回IPV4地址数字
+     * @param bool $adv  是否进行高级模式获取（有可能被伪装）
+     * @return mixed
+     */
+    function spk_get_client_ip($type = 0, $adv = false) {
+        $type = $type? 1 : 0;
+        if($adv) {
+            if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+                $pos = array_search('unknown', $arr);
+                if (false !== $pos) {
+                    unset($arr[$pos]);
+                }
+                $ip = trim($arr[0]);
+            } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+            } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+                $ip = $_SERVER['REMOTE_ADDR'];
+            }
+        } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        // IP地址合法性验证
+        $long = sprintf('%u', ip2long($ip));
+        $ip = $long? array($ip, $long) : array('0.0.0.0', 0);
+
+        return $ip[$type];
+    }
+}
+
+if(!function_exists('spk_get_address_by_ip')) {
+    /**
+     * 根据IP地址获取地理位置（国家、地区、省、市、运营商）
+     * @param $ip IP地址
+     * @return 地理位置数组
+     */
+    function spk_get_address_by_ip($ip = null) {
+        $ip = $ip? : spk_get_client_ip();
+        $address = spk_get_http_response_get("http://ip.taobao.com/service/getIpInfo.php?ip={$ip}");
+        $address = json_decode($address, true);
+        return empty($address['data'])? [] : $address['data'];
+    }
+}
+
 if(!function_exists('spk_is_valide_x')) {
     /**
      * 正则验证指定内容的合法性
--- test.php
+++ test.php
@@ -2,7 +2,10 @@
 
 /* 简单的测试脚本 */
 
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
 header("Content-type: text/html; charset=utf-8");
 include 'helpers.php';
 
-spk_dd(spk_is_valide_x('email', 'liweijsj@163.com'));
+spk_dd(spk_get_address_by_ip('61.158.147.122'));
